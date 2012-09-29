6d13cc564daa71132d1dbb8c7c792ef18cea2e2b
Audio <horkej@hotmail.com>
2012-09-29 23:46:58 +0200

    Typo in ClientException class.

--- src/Client/ClientException.java
+++ src/Client/ClientException.java
@@ -1,28 +1,14 @@
 package Client;
 
-/**
- * Vlastní typ výjimky, vyhozený při chybě programu. Standardní zpracování.
- *
- * @author Martin Fouček
- */
+
 public class ClientException extends Exception {
 
-    /**
-     * Zpracování výjimky.
-     *
-     * @param str
-     */
-    public ClientException (String str) {
+    public ClientException(String str) {
         super(str);
         ClientLogger.log("ClientException: " + str, ClientLogger.ERROR);
     }
 
-    /**
-     * Zpracování všeho, co se dá vyhodit.
-     *
-     * @param obj
-     */
-    public ClientException (Throwable obj) {
+    public ClientException(Throwable obj) {
         super(obj);
         ClientLogger.log("ClientException: " + obj.toString(), ClientLogger.ERROR);
     }
