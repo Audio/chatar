6885d53a5eea989e5f2fb29d43ee1055fdd0c495
Audio <horkej@hotmail.com>
2012-09-30 22:51:50 +0200

    Typo in the Prefix class.

--- src/Connection/Prefix.java
+++ src/Connection/Prefix.java
@@ -1,68 +1,44 @@
 package Connection;
 
+
 /**
- * Prefix odpovedi. Obsahuje az 3 slozky:
+ * Response prefix. Contains up to 3 parts:
  * <servername> | <nick> [ '!' <user> ] [ '@' <host> ]
- * zdroj: http://www.irchelp.org/irchelp/rfc/chapter2.html#c2_3_1
- *
- * @author Martin Fouček
+ * source: http://www.irchelp.org/irchelp/rfc/chapter2.html#c2_3_1
  */
 public class Prefix {
 
-    /**
-     * Odesílatel zprávy, obsahuje všechny informační složky zprávy.
-     */
-    public String prefix;
-    /**
-     * Přezdívka odesílatele.
-     */
+    public String originalPrefix;
     public String nick;
-    /**
-     * Uživatelské jméno odesílatele.
-     */
-    public String user;
-    /**
-     * Hostitelské jméno  odesílatelova počítače.
-     */
-    public String host;
+    public String username;
+    public String hostname;
 
-    /**
-     * Parsuje prefix na jednotlive casti.
-     *
-     * @param prefix
-     */
+
     public Prefix(String prefix) {
-
-        this.prefix = prefix;
+        originalPrefix = prefix;
 
         int upto;
 
-        // cast <nick>
-        if ((upto = prefix.indexOf("!")) < 0) {
+        // the <nick> part
+        if ( (upto = prefix.indexOf("!")) < 0 ) {
             nick = prefix;
             return;
         }
 
-        // cast <user>
+        // the <user> part
         nick = prefix.substring(0, upto);
-        user = prefix.substring(upto + 1);
+        username = prefix.substring(upto + 1);
 
-        // cast <host>
-        if ((upto = user.indexOf("@")) > -1) {
-            host = user.substring(upto + 1);
-            user = user.substring(0, upto);
+        // the <host> part
+        if ( (upto = username.indexOf("@")) > -1 ) {
+            hostname = username.substring(upto + 1);
+            username = username.substring(0, upto);
         }
-
     }
 
-    /**
-     * Vraci puvodni (nezmeneny) prefix odpovedi.
-     *
-     * @return
-     */
     @Override
-    public String toString () {
-        return prefix;
+    public String toString() {
+        return originalPrefix;
     }
 
 }
