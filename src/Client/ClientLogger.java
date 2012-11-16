2c9b8b81c2d18ef899a1e5d7672e66ef1fc7f10f
Audio <horkej@hotmail.com>
2012-11-16 19:35:42 +0100

    ClientLogger: attempt to create log directory if not exist

--- src/Client/ClientLogger.java
+++ src/Client/ClientLogger.java
@@ -1,6 +1,7 @@
 package Client;
 
 import java.io.IOException;
+import java.nio.file.*;
 import java.util.logging.*;
 
 
@@ -8,25 +9,37 @@ public final class ClientLogger {
 
     private static Logger logger;
     private static Handler handler;
+    private static boolean loggingEnabled;
 
     public static final int INFO    = 1;
     public static final int WARNING = 2;
     public static final int ERROR   = 3;
 
     static {
+        initialize();
+    }
 
+    private static void initialize() {
+        Path logDir = FileSystems.getDefault().getPath("./log");
         try {
+            if ( !Files.isDirectory(logDir) )
+                Files.createDirectory(logDir);
+
             logger = Logger.getLogger("Client");
             handler = new FileHandler("log/error.log", true);
             handler.setFormatter( new SimpleFormatter() );
             logger.addHandler(handler);
+            loggingEnabled = true;
         } catch (IOException e) {
+            loggingEnabled = false;
             System.err.println("Logování do souboru je zakázáno.");
         }
-
     }
 
     public static void log(String message, int type) {
+        if (!loggingEnabled)
+            return;
+
         Level realType;
 
         switch (type) {
@@ -44,7 +57,8 @@ public final class ClientLogger {
     }
 
     public static void quit() {
-        handler.close();
+        if (loggingEnabled)
+            handler.close();
     }
 
 }
